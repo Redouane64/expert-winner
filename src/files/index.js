c4456717c726b8b130df4537f6b9ddd06cb70799
Redhouane Sobaihi <redouane.sobaihi@live.com>
2021-09-15 15:43:13 +0000

    feat(): app structure

---
+++ src/files/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const filesRoute = express.Router({
+   // TODO: add any options
+})
+
+// TODO: define files routes
+
+module.exports = filesRoute
---
+++ src/users/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const usersRoute = express.Router({
+   // TODO: add any options
+})
+
+// TODO: define users routes
+
+module.exports = filesRoute
