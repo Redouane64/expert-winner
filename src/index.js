b39fd9952accb357b0fa3f6e99a58c94a5af44dd
Redhouane Sobaihi <redouane.sobaihi@live.com>
2021-09-16 09:22:08 +0000

    feat(): start server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 
+const HOST = process.env.HOST || '127.0.0.1'
+const PORT = process.env.PORT || 5000
+
 const app = express()
 
-
-app.listen()
+app.listen(PORT, HOST, () => {
+    console.info(`Server runnin at ${HOST}:${PORT}`)
+})
