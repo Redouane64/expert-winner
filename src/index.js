b228e408292d70371d715f6f66832f2199dc15b4
Redhouane Sobaihi <redouane.sobaihi@live.com>
2021-09-16 09:35:43 +0000

    feat(): add cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 
 const HOST = process.env.HOST || '127.0.0.1'
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+app.use(cors({
+    methods: [ 'GET', 'HEAD', 'POST' ],
+}))
 
 app.listen(PORT, HOST, () => {
     console.info(`Server runnin at ${HOST}:${PORT}`)
