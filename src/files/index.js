fa19d2856870cbbbd812592aa6547c13c671a2a6
Redouane S <redouane.sobaihi@live.com>
2021-09-18 00:27:59 +0700

    feat(): add multer to parse uploaded file

--- src/files/index.js
+++ src/files/index.js
@@ -1,15 +1,25 @@
 const express = require('express')
+const multer = require('multer')
 
 const createFile = require('./handlers/createFile')
 const getFile = require('./handlers/getFile')
 
 const routePrefix = '/files'
-const parseForm = express.raw({ 
-   type: 'multipart/form-data'
-});
+const upload = multer({
+   limits: {
+      fileSize: 1024 * 1024 * 8 // 8mb
+   },
+   fileFilter: (request, file, callback) => {
+      if (file.mimetype === 'image/png') {
+         callback(null, true)
+      }
+
+      callback(null, false)
+   }
+})
 const filesRoute = express.Router()
 
-filesRoute.post(routePrefix, parseForm, createFile)
-filesRoute.get(routePrefix, parseForm, getFile)
+filesRoute.post(routePrefix, upload.single('file'), createFile)
+filesRoute.get(`${routePrefix}/:filename`, getFile)
 
 module.exports = filesRoute
