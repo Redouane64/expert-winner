6fa968db79a056465cc291c4d0a110c545f8a888
Redouane S <redouane.sobaihi@live.com>
2021-09-18 00:28:53 +0700

    feat(): implement upload and get file endpoints

--- src/files/handlers/createFile.js
+++ src/files/handlers/createFile.js
@@ -1,4 +1,36 @@
+const { randomUUID } = require('crypto')
+const { redisClient } = require("../../common")
 
-module.exports = (request, response) => {
-    return response.send({ ok: true })
+/**
+ * create user
+ * @param {import("express").Request} request 
+ * @param {import("express").Response} response 
+ * @returns user response
+ */
+module.exports = async (request, response) => {
+
+    const { file } = request
+
+    if (!file) {
+        return response.status(400).send({ error: 'file is missing'})
+    }
+
+    try {
+        const fileMetadataKey = randomUUID()
+        const fileKey = randomUUID()
+
+        // save file metadata entry
+        await redisClient.set(fileMetadataKey, JSON.stringify({ file: fileKey }))
+
+        // save file
+        await redisClient.set(fileKey, file.buffer.toString('base64')) 
+        
+        /* response.header('Location', file-path) */
+
+        return response.status(201).send({
+            filename: fileMetadataKey
+        })
+    } catch(error) {
+        return response.status(500).send({ error: 'something went wrong :('})
+    }
 }
--- src/files/handlers/getFile.js
+++ src/files/handlers/getFile.js
@@ -1,4 +1,28 @@
+const { redisClient } = require("../../common")
 
-module.exports = (request, response) => {
-    return response.send({ ok: true })
+/**
+ * create user
+ * @param {import("express").Request} request 
+ * @param {import("express").Response} response 
+ * @returns user response
+ */
+module.exports = async (request, response) => {
+    const metadataKey  =request.params.filename
+
+    if (!metadataKey) {
+        return response.status(400).send({ error: 'invalid filename' })
+    }
+
+    const metadata = await redisClient.get(metadataKey)
+    const { file } = JSON.parse(metadata)
+
+    const raw = await redisClient.get(file)
+
+    if (!raw) {
+        return response.status(404).send({ error: 'no such file' })
+    }
+
+    return response.status(200)
+                   .contentType('image/png')
+                   .end(Buffer.from(raw, 'base64'), 'binary')
 }
